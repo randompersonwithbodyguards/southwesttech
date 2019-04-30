44ffb20f5be8b97ac4ea3f4faad1a936b243ecc1
randompersonwithbodyguards <42543560+randompersonwithbodyguards@users.noreply.github.com>
2019-04-30 08:56:35 -0600

    Add files via upload

--- chapter 18/greatestCommon.java
+++ chapter 18/greatestCommon.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+class greatestCommon {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter first integer: ");
+		int firstInt = input.nextInt();
+		System.out.println("Enter the second Integer: ");
+		int secondInt = input.nextInt();
+		System.out.println("The Greatest Common Divisor is: ");
+		int gcf = recursiveGCD(firstInt, secondInt);
+		System.out.println(gcf);
+	}
+	public static int recursiveGCD(int m, int n){
+		if (m%n == 0){
+			return n;
+		}
+		else {
+			return recursiveGCD(n, m%n);
+		}
+	}
+}
